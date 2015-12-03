57b92882005e6fd255f80d9ea2dffca4e6d243f2
AdrianMLin <adrian.mh.lin@gmail.com>
2015-12-03 11:10:25 -0500

    accounts can now be made

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
   def create
+    u = User.create(
+      email: params['email'].downcase,
+      password: params['password'],
+      image_url: params['image_url']
+    )
+
+    redirect_to '/'
   end
 
-  def show
+  def show    
   end
 
   def new
+    render :signup
   end
 
   def delete
